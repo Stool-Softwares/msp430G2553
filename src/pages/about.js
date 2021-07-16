2b12ff82ed29e5a5adde568acac8696d9316edd7
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-07-16 16:47:54 +0530

    made about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Navbar from '../components/navbar/Navbar'
 
 function About() {
-    return <div>about</div>
+    return (
+        <Fragment>
+            <Navbar />
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '100vw',
+                    height: 'calc(100vh - 70px)',
+                }}
+            >
+                <div style={{ textAlign: 'center' }}>
+                    <h1 style={{ fontSize: 45 }}>About</h1>
+                    <p style={{ fontSize: 23 }}>
+                        In case of any query please contact us{' '}
+                        <a
+                            href='https://github.com/Stool-Softwares'
+                            target='_blank'
+                        >
+                            @stool_softwares
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </Fragment>
+    )
 }
 
 export default About
